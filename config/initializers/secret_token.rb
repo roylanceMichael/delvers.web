c9f54e9371c77d7c436d8cc22668c1b823d5343e
Mike Roylance <roylance.michael@gmail.com>
2013-09-28 12:17:08 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DelversWeb::Application.config.secret_token = 'b93ec4b6a9f9e9fbfe41e1e67dadbb2aa8d0f137ac2f5a7c702a3c809af4709bc346f2119358313fe769ada6d5c49c0da4525e67fb6c6f5e8bfb7d5475fa9b97'
